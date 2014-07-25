410af6f21108534a02f2e80d6ba3257b84c32c1f
Dmitry Morozov <dvmorozov@hotmail.com>
2014-07-25 22:46:06 +0600

    Full list of groups is selected.
    https://www.evernote.com/shard/s132/nl/14501366/81db841e-7191-48c6-9b74-73f2257dc477

--- LinkedIn/LinkedInScanPost/LoadData.js
+++ LinkedIn/LinkedInScanPost/LoadData.js
@@ -34,16 +34,39 @@ function writeSettings() {
 	$.cookie('settings', JSON.stringify(settings));
 }
 
+//	Loads list of groups.
+function loadGroups(forChunk) {
+
+	var f = function (result) {
+		if (isDefined(result) && isDefined(result._count) && isDefined(result._total) && isDefined(result._start) && isDefined(result.values)) {
+			if (result._start === 0 && result._count < result._total) {
+				//	Makes additional requests.
+				for (var start = result._count; start < result._total; start += result._count) {
+					IN.API.Raw('/people/~/group-memberships?count=' + (result._count < result._total - start ? result._count : result._total - start)
+						+ '&start=' + start).result(f);
+				}
+			}
+
+			//	Makes something with the chunk of group list.
+			forChunk(result);
+		}
+	};
+
+	IN.API.Raw("/people/~/group-memberships").result(f);
+}
+
 function loadData() {
 	readSettings();
 
-	//	Loading list of groups.
-	IN.API.Raw("/people/~/group-memberships").result(function (result) {
-		var selected = 0;
-		var received = 0;
-		var text = '';
+	var groups = 0;
+	var selected = 0;
+	var received = 0;
+	var text = '';
 
-		if (isDefined(result) && isDefined(result.values) && isDefined(result._total)) {
+	//	Loads list of groups.
+	loadGroups(function (result) {
+		if (isDefined(result) && isDefined(result.values) && isDefined(result._count)) {
+			groups += result._count;
 
 			for (var i = 0; i < result._count; i++) {
 				var groupId = result.values[i].group.id;
@@ -55,50 +78,51 @@ function loadData() {
 
 						for (var j = 0; j < posts._count; j++) {
 							var summary = posts.values[j].summary;
-                            var newDate = new Date();
+							var newDate = new Date();
 
 							if (isDefined(summary)) {
 								var hasWordInSummary = hasWord(summary);
 
 								if ((isDefined(posts.values[j].title) && hasWord(posts.values[j].title)) ||
-                                    hasWordInSummary) {
-                                    if (hasWordInSummary) {
-                                        //	Marks out the keywords.
-                                        for (var k = 0; k < settings.words.length; k++) {
-                                            var keyword = settings.words[k];
+									hasWordInSummary) {
+									if (hasWordInSummary) {
+										//	Marks out the keywords.
+										for (var k = 0; k < settings.words.length; k++) {
+											var keyword = settings.words[k];
 
-                                            summary = summary.replace(keyword,
-                                                    '<span class="keyword">' + keyword + '</span>');
-                                        }
-                                    }
+											summary = summary.replace(keyword,
+													'<span class="keyword">' + keyword + '</span>');
+										}
+									}
 
-                                    var timestamp = posts.values[j].creationTimestamp;
-                                    if (isDefined(timestamp)) {
-                                        newDate.setTime(timestamp);
+									var timestamp = posts.values[j].creationTimestamp;
+									if (isDefined(timestamp)) {
+										newDate.setTime(timestamp);
 
-                                        if (!isDefined(settings.lastTimeStamp) || timestamp > settings.lastTimeStamp)
-                                            settings.lastTimeStamp = timestamp;
-                                    }
-                                }
+										if (!isDefined(settings.lastTimeStamp) || timestamp > settings.lastTimeStamp)
+											settings.lastTimeStamp = timestamp;
+									}
+								}
 
-                                text += '<div class="panel panel-primary">';
-                                text += '<div class="panel-heading"><h3 class="panel-title">' +
-                                    posts.values[j].title + '</h3></div>';
+								text += '<div class="panel panel-primary">';
+								text += '<div class="panel-heading"><h3 class="panel-title">' +
+									posts.values[j].title + '</h3></div>';
 
-                                text += '<div class="panel-body">' + summary + '</div>';
+								text += '<div class="panel-body">' + summary + '</div>';
 
-                                text += '<div class="panel-footer">';
-                                text += '<a class="url" style="" href="' +
-                                    posts.values[j].siteGroupPostUrl +
-                                    '"><img src="LinkedIn.jpg" alt="LinkedIn logo" height="32" width="32" /></a>';
+								text += '<div class="panel-footer">';
+								text += '<a class="url" style="" href="' +
+									posts.values[j].siteGroupPostUrl +
+									'"><img src="LinkedIn.jpg" alt="LinkedIn logo" height="32" width="32" /></a>';
 
-                                text += '&nbsp;<span class="date">' + newDate.toUTCString() + '</span>';
-                                text += '</div></div>';
+								text += '&nbsp;<span class="date">' + newDate.toUTCString() + '</span>';
+								text += '</div></div>';
 
-                                ++selected;
-                                document.getElementById('search').innerHTML = text;
-                                document.getElementById('selected').innerHTML = 'Selected: ' + selected;
-                                document.getElementById('new').innerHTML = 'New: ' + received;
+								++selected;
+								document.getElementById('search').innerHTML = text;
+								document.getElementById('selected').innerHTML = 'Selected: ' + selected;
+								document.getElementById('new').innerHTML = 'New: ' + received;
+								document.getElementById('groups').innerHTML = 'Groups: ' + groups;
 							}
 						}
 					}
